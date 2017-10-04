fcc7fce33269dc5632e6a5697ddc49e751a261e6
mblackhamgw <morrisb@xmission.com>
2017-10-04 16:10:45 -0600

    Update setup.py

--- helpdesk/management/commands/setup.py
+++ helpdesk/management/commands/setup.py
@@ -152,6 +152,8 @@ Thanks and good luck with the application.
             newline = "ALLOWED_HOSTS = ['%s']" % ipaddr
         elif host and not ipaddr:
             newline = "ALLOWED_HOSTS = ['%s']" % host
+            
+        print "allowed line = %s" % newline
 
         if ipaddr != None:
             print "Using IP address: %s " % ipaddr
